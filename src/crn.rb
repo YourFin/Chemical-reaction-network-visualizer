c3e94af5cea2ac29be04c611ac11a51c7631f57f
YourFin <d.junkpobox+github@gmail.com>
2017-11-19 18:03:55 -0600

    Start on molecule class

--- src/crn.rb
+++ src/crn.rb
@@ -1,1 +1,21 @@
 #!/bin/env ruby
+
+import 'gosu'
+
+class molecule
+  def initialize(x_pos, y_pos, x_vel, y_vel)
+    @x_start_pos = x_pos
+    @y_start_pos = y_pos
+    @x_start_vel = x_vel
+    @y_start_vel = y_vel
+    @x_pos = x_pos
+    @y_pos = y_pos
+    @x_vel = x_vel
+    @y_vel = y_vel
+    @bouncelist = []
+  end
+  def initialize(x_pos, y_pos, x_vel, y_vel, bouncelist)
+    initialize(x_pos, y_pos, x_vel, y_vel)
+    @bouncelist = bouncelist
+  end
+end
