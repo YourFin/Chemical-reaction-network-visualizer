5e9b170dafa852ab5a107110afe5afa0019d7148
YourFin <d.junkpobox+github@gmail.com>
2017-11-27 18:21:24 -0600

    Removed random import, fixed some code

--- src/crn.rb
+++ src/crn.rb
@@ -1,20 +1,20 @@
 #!/bin/env ruby
 
 require 'gosu'
-require 'random'
+
+class CRN
+  def initialize()
+    @species_list = []
+    @molecules = []
+  end
+end
 
 class Species
-  def initialize(name, initial_count)
-    initialize(name, initial_count, Gosu::Color.from_hsv(Random.rand(360), 1, 1))
-  end
-  def initialize(name, inital_count, color = "")
+  def initialize(name, inital_count, color = Gosu::Color.from_hsv(Random.rand(360), 1, 1))
   end
 end
 
 class Molecule
-  def initialize(x_pos, y_pos, x_vel, y_vel, species)
-    initialize(x_pos, y_pos, x_vel, y_vel, species, [])
-  end
-  def initialize(x_pos, y_pos, x_vel, y_vel, species, bouncelist)
+  def initialize(x_pos, y_pos, x_vel, y_vel, species, bouncelist=[])
   end
 end
