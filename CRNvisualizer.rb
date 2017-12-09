e00b2088079e26e5ab0d211269332c246b6b35a4
YourFin <d.junkpobox+github@gmail.com>
2017-12-08 20:38:37 -0600

    Added main file to run from

---
+++ CRNvisualizer.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require './src/crn.rb'
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: CRNvisualizer CRN_TO_VISUALIZE.cps [OPTIONS]"
+  opts.separator  ""
+  opts.separator  "Options:"
+  opts.on("-h", "--help", "Display this message and exit.") do
+    puts opts
+    exit
+  end
+end.parse!
+
+def invalidArguments()
+  puts "Invalid arguments; try 'CRNvisualizer --help' for help"
+  exit 1
+end
+
+#begin
+  crn = CRN.parseFromCPSFile(ARGV[0])
+#rescue
+#  puts 'Invalid CPS file'
+#  invalidArguments
+#end
+
